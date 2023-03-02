49c3c1cf2a5c8046c551ebf7ae023d98097a1736
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-03-02 16:02:45 +0700

    using template

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "halo")
+		var data = map[string]string{
+			"Name":    "john wick",
+			"Message": "have a nice day",
+		}
+
+		var t, err = template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(w, data)
 	})
 
 	http.HandleFunc("/index", index)
