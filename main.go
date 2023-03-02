29c67b0225dece2266891c1d5109424596bb8d70
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-03-02 22:42:02 +0700

    add struct and sample

--- main.go
+++ main.go
@@ -1,29 +1,14 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
-func main() {
-	var jsonString = `[
-		{"Name": "john wick", "Age": 27},
-		{"Name": "ethan hunt", "Age": 32}
-	]`
-
-	var data []User
-
-	var err = json.Unmarshal([]byte(jsonString), &data)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println("user 1:", data[0].FullName)
-	fmt.Println("user 2:", data[1].FullName)
+type student struct {
+	ID    string
+	name  string
+	grade int
 }
 
-type User struct {
-	FullName string `json:"Name"`
-	Age      int
+var data = []student{
+	student{"E001", "ethan", 21},
+	student{"W001", "wick", 22},
+	student{"B001", "bourne", 23},
+	student{"B002", "bond", 23},
 }
