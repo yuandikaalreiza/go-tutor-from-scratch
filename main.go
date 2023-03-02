0ff3ec32182028ee5f976231e0be38adbac48913
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-03-02 22:30:05 +0700

    decode JSON ke map[string]interface{}

--- main.go
+++ main.go
@@ -9,19 +9,8 @@ func main() {
 	var jsonString = `{"Name": "john wick", "Age": 27}`
 	var jsonData = []byte(jsonString)
 
-	var data User
-
-	var err = json.Unmarshal(jsonData, &data)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println("user :", data.FullName)
-	fmt.Println("age :", data.age)
-}
-
-type User struct {
-	FullName string `json:"name"`
-	age      int
+	var data1 map[string]interface{}
+	json.Unmarshal(jsonData, &data1)
+	fmt.Println("user :", data1["Name"])
+	fmt.Println("age  :", data1["Age"])
 }
