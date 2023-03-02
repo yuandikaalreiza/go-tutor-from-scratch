07ec647f23e2424d2b6b93b283a9bf9181089662
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-03-02 22:53:23 +0700

    add /user endpoint

--- main.go
+++ main.go
@@ -35,3 +35,31 @@ func users(w http.ResponseWriter, r *http.Request) {
 
 	http.Error(w, "", http.StatusBadRequest)
 }
+
+func user(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		var id = r.FormValue("id")
+		var result []byte
+		var err error
+
+		for _, each := range data {
+			if each.ID == id {
+				result, err = json.Marshal(each)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				w.Write(result)
+				return
+			}
+		}
+
+		http.Error(w, "User not found", http.StatusBadRequest)
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+}
