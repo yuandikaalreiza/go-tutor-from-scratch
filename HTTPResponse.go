4abe2b1d2577084a0c819c3d5bdbae112480ec8b
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-03-03 13:29:40 +0700

    Update HTTPResponse.go

--- HTTPResponse.go
+++ HTTPResponse.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -18,7 +19,7 @@ func fetchUsers() ([]student, error) {
 	var client = &http.Client{}
 	var data []student
 
-	request, err := http.NewRequest("POST", baseURL+"/users", nil)
+	request, err := http.NewRequest("GET", baseURL+"/users", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -35,4 +36,16 @@ func fetchUsers() ([]student, error) {
 	}
 
 	return data, nil
+}
+
+func main() {
+	var users, err = fetchUsers()
+	if err != nil {
+		fmt.Println("Erro njer!", err.Error())
+		return
+	}
+
+	for _, each := range users {
+		fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", each.ID, each.Name, each.Grade)
+	}
 }
