83e8091389fd0e36cd8981e122ddda2d654e09cb
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-03-03 21:14:06 +0700

    rename fetchUsers

--- fetchUsers.go
+++ fetchUsers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 var baseURL = "http://localhost:8080"
@@ -36,6 +38,36 @@ func fetchUsers() ([]student, error) {
 	}
 
 	return data, nil
+}
+
+func fetchUser(ID string) (student, error) {
+	var err error
+	var client = &http.Client{}
+	var data student
+
+	var param = url.Values{}
+	param.Set("id", ID)
+	var payload = bytes.NewBufferString(param.Encode())
+
+	request, err := http.NewRequest("POST", baseURL+"/user", payload)
+	if err != nil {
+		return data, err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return data, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
+
 }
 
 func main() {
